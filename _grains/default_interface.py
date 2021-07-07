21ce22ee0300941a776e478088d9e8bc28bc625f
Marius Kittler <mkittler@suse.de>
2021-07-07 14:39:19 +0200

    Fix `_grains/default_interface.py` for Python 3
    This should fix one of the backtraces we see in production (see
    https://progress.opensuse.org/issues/94994).
    
    The problem has also been reported upstream (see
    https://github.com/saltstack/salt/issues/58860) and apparently the solution
    is to use `salt.utils.network` directly instead of `salt.modules.network`.
    This change implements that.
    
    ---
    
    Tested by appending `print(default_interface())` at the end of the grain
    and invoking it locally via `python3 _grains/default_interface.py`.
    
    Before the change an error message similar to the one from production
    occurs:
    
    ```
    Traceback (most recent call last):
      File "_grains/default_interface.py", line 24, in <module>
        print(default_interface())
      File "_grains/default_interface.py", line 10, in default_interface
        internalip = salt.modules.network.ip_addrs(cidr='10.160.0.0/13')[0]
      File "/usr/lib/python3.8/site-packages/salt/modules/network.py", line 1278, in ip_addrs
        addrs = __utils__["network.ip_addrs"](
    NameError: name '__utils__' is not defined
    ```
    
    With the change it returns the interface used to connect to SUSE's internal
    network (which seems to be the intention of the code):
    
    ```
    {'default_interface': 'tun0'}
    ```
    
    (Since I'm only connected via VPN to the internal network, `tun0` is the
    correct interface in my local test.)

--- _grains/default_interface.py
+++ _grains/default_interface.py
@@ -1,16 +1,52 @@
 #!/usr/bin/env python
-import salt.modules.network
 
+# use salt.utils.network directly (instead of salt.modules.network) to workaround
+# https://github.com/saltstack/salt/issues/58860 and https://progress.opensuse.org/issues/94994
+import salt.utils.network
+
+def ip_addrs(interface=None, include_loopback=False, cidr=None, type=None):
+    """
+    Returns a list of IPv4 addresses assigned to the host. 127.0.0.1 is
+    ignored, unless 'include_loopback=True' is indicated. If 'interface' is
+    provided, then only IP addresses from that interface will be returned.
+    Providing a CIDR via 'cidr="10.0.0.0/8"' will return only the addresses
+    which are within that subnet. If 'type' is 'public', then only public
+    addresses will be returned. Ditto for 'type'='private'.
+
+    .. versionchanged:: 3001
+        ``interface`` can now be a single interface name or a list of
+        interfaces. Globbing is also supported.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' network.ip_addrs
+
+    This function is taken from salt.modules.network and has been adjusted
+    for not relying on `__utils__` (see issues referenced by comment on
+    top).
+    """
+    addrs = salt.utils.network.ip_addrs(interface=interface, include_loopback=include_loopback)
+    if cidr:
+        return [i for i in addrs if salt.utils.network.in_subnet(cidr, [i])]
+    else:
+        if type == "public":
+            return [i for i in addrs if not is_private(i)]
+        elif type == "private":
+            return [i for i in addrs if is_private(i)]
+        else:
+            return addrs
 
 def default_interface():
     """Return the default network interface name from a machine based on its IPv4 configuration."""
     grains = {'default_interface': None}
     # Use first IP which matches the SUSE internal network
     try:
-        internalip = salt.modules.network.ip_addrs(cidr='10.160.0.0/13')[0]
+        internalip = ip_addrs(cidr='10.160.0.0/13')[0]
     except IndexError:
         return grains
-    interfaces = salt.modules.network.interfaces()
+    interfaces = salt.utils.network.interfaces()
 
     # Iterate over all interfaces and see which one contains the IP from above first
     for interface, data in interfaces.items():
