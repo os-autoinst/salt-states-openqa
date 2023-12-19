0d0a39f782e5573e5af0fdfd0b311af0a859cc3f
mkittler <marius.kittler@suse.com>
2023-12-19 10:10:17 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- openqa/auto-update.sh
+++ openqa/auto-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 zypper_patch() {
-    ret=0
+    local ret=0
     zypper -n --no-refresh --non-interactive-include-reboot-patches patch --replacefiles --auto-agree-with-licenses --download-in-advance || ret=$?
     [[ $ret == 102 ]] && ret=0 # don't interpret exit code [102 - ZYPPER_EXIT_INF_REBOOT_NEEDED] as an error
     return $ret
