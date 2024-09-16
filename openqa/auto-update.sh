3fc9d2d3bfa871b8e278ba7b401aa315614552c0
Oliver Kurz <okurz@suse.de>
2024-09-16 15:20:58 +0200

    auto-update: Fix application of conflicting patches
    As encountered in https://progress.opensuse.org/issues/166313 apparently
    there can be official patches still causing conflicts which would need
    manual resolution or forcing a resolution. This commit looks into the
    exit code of zypper of the patch command and if "4" is encountered as
    observed on conflicting patches then another run is triggered with the
    "--force-resolution" option. This option can be harmful but here I
    consider it safe enough considering that official system repositories
    from where patches can come would not trigger inconsistencies as we have
    observed in other cases.
    
    Related progress issue: https://progress.opensuse.org/issues/166313

--- openqa/auto-update.sh
+++ openqa/auto-update.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -ex
 
+zypper_args=(-n --no-refresh --non-interactive-include-reboot-patches patch --replacefiles --auto-agree-with-licenses --download-in-advance)
 zypper_patch() {
     local ret=0
-    zypper -n --no-refresh --non-interactive-include-reboot-patches patch --replacefiles --auto-agree-with-licenses --download-in-advance || ret=$?
+    zypper "${zypper_args[@]}" || ret=$?
+    # In case of conflicting patches try to force a resolution which could be
+    # dangerous if non-official repositories are not available but here we
+    # "only" call it on official patches
+    # See https://progress.opensuse.org/issues/166313
+    if [[ $ret == 4 ]]; then
+        zypper "${zypper_args[@]}" --force-resolution || ret=$?
+    fi
     [[ $ret == 102 ]] && ret=0 # don't interpret exit code [102 - ZYPPER_EXIT_INF_REBOOT_NEEDED] as an error
     return $ret
 }
