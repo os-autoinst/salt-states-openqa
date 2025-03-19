os-autoinst/salt-states-openqa

commit 01afade22bed83423562183ea2676552e2841e90

Manage and recover openQA NFS mount on libvirt machines
* Avoid having to manually recover the NFS mount on e.g. s390x svirt hosts
* See https://progress.opensuse.org/issues/178972

top.sls
@@ -62,6 +62,8 @@ base:
     - jenkins
   'G@roles:libvirt':
     - libvirt
+    - openqa.nfs_share
+    - openqa.recover-nfs
   'G@roles:external_openqa_hypervisor':
     - libvirt.storage
     - openqa.kvm_firewall

commit 959f95149bf69e809c0d94cf7b05db794b176249

Only manage NFS on openQA hypervisors
01afade2 introduced this to the wrong role and only external openQA
hypervisors actually need this mount point.

Related ticket: https://progress.opensuse.org/issues/178972

top.sls
@@ -62,10 +62,10 @@ base:
     - jenkins
   'G@roles:libvirt':
     - libvirt
-    - openqa.nfs_share
-    - openqa.recover-nfs
   'G@roles:external_openqa_hypervisor':
     - libvirt.storage
     - openqa.kvm_firewall
+    - openqa.nfs_share
+    - openqa.recover-nfs
   'openqa-piworker.qe.nue2.suse.org':
     - raspberrypi