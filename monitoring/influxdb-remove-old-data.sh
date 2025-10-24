4f6c297b626b0b9f1b83e867466ed11f7be4d657
Oliver Kurz <okurz@suse.de>
2025-10-24 13:15:27 +0200

    monitoring: Cleanup biggest influxdb measurement "nginx_log" more aggressively
    Related progress issue: https://progress.opensuse.org/issues/190926

--- monitoring/influxdb-remove-old-data.sh
+++ monitoring/influxdb-remove-old-data.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
-measurements=(nginx_log cpu apache_log procstat disk ping net mem diskio chrony system)
 age="${age:-"180d"}"
-
+age_nginx_log="${age_nginx_log:-"20d"}"
+influx -database telegraf -execute "DELETE FROM nginx_log WHERE time < now() - $age_nginx_log;"
+measurements=(cpu apache_log procstat disk ping net mem diskio chrony system)
 for measurement in "${measurements[@]}"; do
         influx -database telegraf -execute "DELETE FROM $measurement WHERE time < now() - $age;"
 done
