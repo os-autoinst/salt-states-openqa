a2e1779e488c5388be0fc09d1f557a1ce4b8a340
Dominik Heidler <dheidler@suse.de>
2026-04-08 12:18:18 +0200

    salt-keys-check: Fetch backlog info from progress
    Ticket: https://progress.opensuse.org/issues/198233

--- openqa/salt-keys-check.py
+++ openqa/salt-keys-check.py
@@ -5,6 +5,7 @@ import sys
 import json
 import subprocess
 import socket
+import requests
 
 script_path = os.path.abspath(sys.argv[0])
 host = socket.gethostbyaddr(socket.gethostname())[0]
@@ -12,19 +13,42 @@ host = socket.gethostbyaddr(socket.gethostname())[0]
 js = subprocess.check_output(["salt-key", "--out=json"])
 j = json.loads(js)
 
-bad = {
+states = {
     "unaccepted": j["minions_pre"],
     "rejected": j["minions_rejected"],
     "denied": j["minions_denied"],
 }
 
-# dict of hostname -> ticket entries for all hosts with salt-keys in a state different from "accepted"
-MINION_KEYS_NOT_ACCEPTED_REASONS = json.load(open('/etc/salt-keys-check-keys-not_accepted_reasons.json'))
+backlog = requests.get("https://progress.opensuse.org/issues.json?query_id=757").json()
+
+def get_ticket(minion):
+    for issue in backlog["issues"]:
+        if minion in issue["subject"]:
+            return f"https://progress.opensuse.org/issues/{issue['id']}"
+
+if states["rejected"]:
+    print("Minions with rejected keys (this is okay):")
+    for minion in states["rejected"]:
+        print(f"  {minion}")
 
 exit_code = 0
-for status, hostnames in bad.items():
-    for hostname in hostnames:
-        if not hostname in MINION_KEYS_NOT_ACCEPTED_REASONS:
-            print(f"salt-key for {hostname} is {status} with no reason listed in {host}:{script_path}")
+if states["denied"]:
+    print("\nDenied keys:")
+    for minion in states["denied"]:
+        ticket = get_ticket(minion)
+        print(f"  {minion} (Ticket: {ticket})")
+        if not ticket:
             exit_code = 1
+
+if states["unaccepted"]:
+    print("\nUnaccepted keys:")
+    for minion in states["unaccepted"]:
+        ticket = get_ticket(minion)
+        print(f"  {minion} (Ticket: {ticket})")
+        if not ticket:
+            exit_code = 1
+
+if exit_code:
+    print("\nThere are minions with unaccepted/denied keys and no ticket mentioning them!")
+
 sys.exit(exit_code)
